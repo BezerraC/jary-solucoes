b73b45e1eca4c733fd50ada258e0223b37a5a22b
Carlos Bezerra <cbezerraneto@gmail.com>
2022-07-28 15:23:47 -0300

    add php extension on css

--- index.php
+++ index.php
@@ -123,8 +123,9 @@ $body_class = $body_class . $product_class;
 
 	<link rel="shortcut icon" href="/favicon.ico">
   
-  <link type="text/css" rel="stylesheet" href="/css/style.css" />
-  <link type="text/css" rel="stylesheet" href="/css/owera.css" />
+  <link type="text/css" rel="stylesheet" href="/css/style.css.php" />
+  <link type="text/css" rel="stylesheet" href="/css/owera.css.php" />
+  
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
