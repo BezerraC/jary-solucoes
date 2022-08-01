bab98c1966e345e4ad42498ffc3cb341a4e94ef8
Carlos Bezerra <cbezerraneto@gmail.com>
2022-08-01 09:41:53 -0300

    change footer values

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 
     <div class="col-md-4 d-flex align-items-center">
       <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <img src="/img/logo.png" width="115">
+        <img src="/img/logo.png" width="85">
       </a>
-      <span class="text-muted">Jary Soluções Sustentáveis © 2022</span>
+      <span class="text-muted">© 2022</span>
     </div>
 
     <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
