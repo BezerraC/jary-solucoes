614765bbd977f22d00febb500279477103686add
Carlos Bezerra <cbezerraneto@gmail.com>
2022-08-01 09:08:54 -0300

    adding php body

---
+++ header.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light nav-bg">
+  <div class="container-fluid p-3 ">
+    <a class="navbar-brand" href="/"><img src="/img/logo.png" width="115" alt="Jary Soluções Sustentáveis" title="Jary Soluções Sustentáveis" class="d-inline-block align-text-top ms-md-5 ms-2"></a>
+    <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav mb-2 mb-lg-0 d-flex me-md-5 mt-3 mt-md-0 justify-content-end">
+        <li class="nav-item">
+          <a class="nav-title link-style-h menu-home" href="/" title="Home">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-title  link-style-h  menu-sobre-nos" aria-current="page" href="/sobre-nos" title="Sobre Nós">SOBRE NÓS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-title  link-style-h menu-servicos" href="/servicos" title="Serviços">SERVIÇOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-title  link-style-h menu-produtos" href="/produtos" title="Produtos">PRODUTOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-title  link-style-h menu-contato" href="/contato" title="Contato">CONTATO</a>
+        </li>
+        <li class="nav-item ms-2">
+          <a href="http://twitter.com/owera" target="_blank" title="Twitter" style="font-size: 20px;" class="me-3 me-md-0 icon nav-link fa fa-twitter"></a>
+          <a href="http://facebook.com/owerasoftware" target="_blank" title="Facebook" style="font-size: 20px;" class="nav-link fa fa-facebook icon"></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -13,22 +13,16 @@
     <!--Favicon-->
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-             <img src="/img/logo.png" alt="" width="115" class="d-inline-block align-text-top">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
+    <div class="container">
+        <header>
+            <?php include('header.php'); ?>
+        </header>
+        <div role="main" id="main">
+            <?php include($page_file); ?>
         </div>
-        </div>
+        <footer class="clearfix shadow-white">
+            <?php include('footer.php'); ?>
+        </footer>
     </div>
-    </nav>
 </body>
 </html>
