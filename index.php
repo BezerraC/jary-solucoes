e3e23aec4f05d8d00c3ffc176040a299a23e3cac
Carlos Bezerra <cbezerraneto@gmail.com>
2022-08-01 10:28:34 -0300

    add js file

--- index.php
+++ index.php
@@ -55,9 +55,13 @@ setcookie('googtrans', '/pt');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php print $page_title; ?> Jary Soluções Sustentáveis</title>
     <meta name="description" content="Uma nova forma de pensar em desenvolvimento local e sustentabilidade.">
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Icon Library -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!-- CSS File -->
     <link href="/css/style.css" rel="stylesheet">
+    <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 </head>
 <body>
@@ -72,7 +76,9 @@ setcookie('googtrans', '/pt');
             <?php include('footer.php'); ?>
         </footer>
     </div>
-    
+
+    <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="/js/main.js"></script>
 </body>
 </html>
