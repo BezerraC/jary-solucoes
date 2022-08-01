e32715fdb74929d7262414bf500257a91f1b3c70
Carlos Bezerra <cbezerraneto@gmail.com>
2022-08-01 09:39:01 -0300

    add icons libary

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 
     <div class="col-md-4 d-flex align-items-center">
       <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        <img src="/img/logo.png" width="115">
       </a>
-      <span class="text-muted">© 2021 Company, Inc</span>
+      <span class="text-muted">Jary Soluções Sustentáveis © 2022</span>
     </div>
 
     <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/jarisolucoessustentaveis/" target="_blank"><i class="uil uil-instagram"></i></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><i class="uil uil-facebook-f"></i></a></li>
+      <!-- <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li> -->
     </ul>
--- index.php
+++ index.php
@@ -55,6 +55,7 @@ setcookie('googtrans', '/pt');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jary Soluções Sustentáveis</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href="/css/style.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 </head>
