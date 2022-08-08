343168de301d2702216f41d476237d6e627d1762
Carlos Bezerra <cbezerraneto@gmail.com>
2022-08-08 17:46:32 -0300

    home page

--- home.php
+++ home.php
@@ -13,3 +13,49 @@
         </div>
     </div>
 </section>
+
+<section class="px-4 margin_sec">
+    <div class="text-center">
+        <h2 class="title">Sobre Nós</h2>
+        <p class="subtitle">Somos uma empresa focada na elaboração de produtos de base ambiental que tem como principal laboratório a Floresta Amazônica. Estamos localizados no extremo sul do estado do Amapá, no município de Laranjal do Jari. Atualmente estamos produzindo uma ração alternativa para peixes, com alto valor nutricional e de baixo custo de aquisição para os piscicultores. </p>
+    </div>   
+</section>
+
+<section class="px-4 margin_sec">
+    <div class="text-center">
+        <h2 class="title">Produtos</h2>
+        <p class="subtitle">Entregamos ao piscicultor uma ração de qualidade e com a originalidade amazônica.</p>
+    </div>
+    <div class="mt-5"></div>
+    <div class="row prod">
+            <div class="col d-flex justify-content-center">
+                <img src="/img/racao1.jpeg" width="350">
+            </div>
+            <div class="col d-flex justify-content-center mt-4 mt-sm-0">
+                <img src="/img/racao2.jpeg" width="350">
+            </div>
+            <div class="col d-flex justify-content-center mt-4 mt-sm-0">
+                <img src="/img/racao3.jpeg" width="350">
+            </div>
+    </div>
+    <div class="mt-5">
+        <p class="subtitle">A Ração Vale do Jari, nasceu para atender as necessidades do pequeno piscicultor do Vale do Jari em adquirir uma ração de alto valor nutricional e que seja a cara da Amazônia. Assim, estamos preparando uma ração alternativa com base na cadeia produtiva dos frutos e frutas amazônicas que são utilizados in natura na alimentação dos peixes. Nosso processo passou por rigorosas análises físicas, químicas e microbiológicas para que o produto final esteja de acordo com as especificações necessárias para uma alimentação saudável para os peixes.
+        <br><br>Um dos componentes da Ração Vale do Jari é o buriti, Mauritia flexuosa L. Esse fruto é muito utilizado nas regiões da Amazônia e em algumas áreas do cerrado brasileiro. Essa espécie possui uma grande variedade de nutrientes, proteínas, ácidos graxos e vitaminas como A, B, C. Observa-se ainda que este fruto é bastante utilizado pelas comunidades tradicionais locais, apresentam assim um enorme potencial biotecnológico. Os demais componentes da ração também fazem parte da cadeia amazônica.
+        </p>
+    </div>
+   
+</section>
+
+<section class="px-4 margin_sec">
+    <div class="text-center">
+        <h2 class="title">Nosso Time</h2>
+        <p class="subtitle">Entregamos ao piscicultor uma ração de qualidade e com a originalidade amazônica.</p>
+    </div>
+</section>
+
+<section class="px-4 margin_sec">
+    <div class="text-center">
+        <h2 class="title">Contato</h2>
+        <p class="subtitle">Fique a vontade para entrar em contato conosco</p>
+    </div>
+</section>
